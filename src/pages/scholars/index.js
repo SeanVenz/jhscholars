c019137574c67860a600b67e8b4819adc8ba5b95
Sean <seanvenz.quijano@cit.edu>
2024-09-14 21:18:30 +0800

    Refactor card2 component and remove unused files

--- src/pages/scholars/index.js
+++ src/pages/scholars/index.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import MyCard from '../../components/card'
-import Navbar from '../../components/navbar'
-import Button from '../../components/button'
+import React from "react";
+import MyCard from "../../components/card";
+import Navbar from "../../components/navbar";
+import Button from "../../components/button";
+import "./index.scss";
 
 function Scholars() {
   return (
     <>
-    <Navbar></Navbar>
-      <h5>Meet Our Top Scholars!</h5>
-      <MyCard></MyCard>
-      
-      <h4>Need an axie? We've got you covered</h4>
-      <p>
-        Our scholars enjoy access to exclusive Axie rental vouchers! Unlock your
-        potential by renting the best Axies in the game and climb the
-        leaderboards with ease!
-      </p>
-      <p>Browse our axies here!</p>
-      <Button></Button>
+      <div className="scholars">
+        <Navbar></Navbar>
+        <h2>Meet Our Top Scholars!</h2>
+        <MyCard></MyCard>
+
+        <div className="info">
+          <h4>Need an axie? We've got you covered</h4>
+          <p>
+            Our scholars enjoy access to exclusive Axie rental vouchers! Unlock
+            your potential by renting the best Axies in the game and climb the
+            leaderboards with ease!
+          </p>
+          <p>Browse our axies here!</p>
+          <div className="btn">
+          <Button></Button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Scholars
+export default Scholars;
