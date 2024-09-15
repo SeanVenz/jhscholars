fdb4d17dcd7b4cef181f02914b8b1f8e39433bf8
Sean <seanvenz.quijano@cit.edu>
2024-09-15 13:26:10 +0800

    Refactor aboutus page content structure and styles

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -41,22 +41,27 @@ function AboutUs() {
       <Navbar />
       <div className="content">
         <h3>Welcome to the JH Community Guild!</h3>
-        <p>
-          We are a guild that thrives on unity, competition, and growth. Our
-          scholars are empowered through exciting challenges,
-          <br /> generous rewards, and access to the best tools and support to
-          maximize their performance in the world of Axie Classic.
-          <br /> Whether you're here to compete or collaborate, there's a place
-          for you in our ever-growing community.
-        </p>
+
+        <div className="text">
+          <p>
+            We are a guild that thrives on unity, competition, and growth. Our
+            scholars are empowered through exciting challenges, generous
+            rewards, and access to the best tools and support to maximize their
+            performance in the world of Axie Classic. Whether you're here to
+            compete or collaborate, there's a place for you in our ever-growing
+            community.
+          </p>
+        </div>
 
         <h4>Season Rewards & Incentives</h4>
-        <p>
-          We value the hard work of our scholars! The size of our season rewards
-          is tied to the size of our community.
-          <br /> When we hit key milestones like 60 members, our rewards grow
-          even bigger! Check out the prizes for Season 5:
-        </p>
+        <div className="text">
+          <p>
+            We value the hard work of our scholars! The size of our season
+            rewards is tied to the size of our community. When we hit key
+            milestones like 60 members, our rewards grow even bigger! Check out
+            the prizes for Season 5:
+          </p>
+        </div>
         <div className="rewards">
           {rewards.map((reward, index) => (
             <ul key={index}>
@@ -67,7 +72,7 @@ function AboutUs() {
             </ul>
           ))}
         </div>
-        <Button link="/scholars" text="TOP SCHOLARS"></Button>
+        <Button link="/scholars" text="SCHOLARS"></Button>
       </div>
     </div>
   );
